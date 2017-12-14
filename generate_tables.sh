01c1557acdb8750a547d1be49c687b5a8ff91cd5
moritz <moritz.fickenscher@fau.de>
2017-12-14 22:02:30 +0100

    really only use rectangles for splitup

--- generate_tables.sh
+++ generate_tables.sh
@@ -2,6 +2,7 @@
  # generates markdown tables for n in range [1,40] with limit n+1000 for m
 for i in `seq 1 40`;
 do
+	echo calc n=$i
 	python generate_triple_table_for_n.py $i > results/$i.md
 done
 
